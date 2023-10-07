cd0d5d0de1b8a83253efc4b1d4b8cd98bd5d0ded
Lawrita <143140830+Lawrita@users.noreply.github.com>
2023-10-07 08:39:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,132 @@
+
+
+// create an object for 5 students that are writing jamb(firstname,lastname,email,phone,address,age,gender,score)
+const students1={
+    firstName: "lawrita",
+    lastName: "george",
+    email: "georgediamond@gmail.com",
+    age: "40",
+    phoneNumber: "123456678",
+    gender: "female",
+    score: "48",
+}
+const student2={
+    firstName: "ibiso",
+    lastName: "tonye",
+    email: "diamondg@gmail.com",
+    age: "49",
+    phoneNumber: "1234567858",
+    gender: "male",
+    score: "60",
+}
+
+const student3={
+    firstName: "ibierembo",
+    lastName: "awo",
+    email: "georged@gmail.com",
+    age: "54",
+    phoneNumber: "1234112678",
+    gender: "female",
+    score: "90",
+}
+
+const student4={
+    firstName: "smith",
+    lastName: "boma",
+    email: "georgediamond@gmail.com",
+    age: "80",
+    phoneNumber: "123456678",
+    gender: "female",
+    score: "98",
+}
+
+const student5={
+    firstName: "harry",
+    lastName: "henry",
+    email: "georgd@gmail.com",
+    age: "40",
+    phoneNumber: "0903456678",
+    gender: "male",
+    score: "79",
+}
+
+ console.log(students1)
+ console.log(student2)
+console.log(student3)
+console.log(student4)
+console.log(student5)
+
+
+
+// create an array of 15 items
+const typesOfHair={namesOfHairs:[
+    "virginHair","remy hair", "brazilianHair","virgin peruvian hair",
+     "peruvian curl","peruvian curlly type","virgin indian hair","human hir",
+     "malaysian hair", "braided wigs","ghana braiding", "upgel","allback","dreadlocs","frontalwigs"],
+colorsOfHairs:[
+  "green", "black","brown","purple","orange","blue","white","blond","color 33","color 27","lemon",
+   "red","oxblood","yellow","gray",
+],
+typesOfCurls:[
+    "jerry curls","water curls","ringlet curls","deepwaves","water waves","afro kinky curls","italian curls",
+    "funmi curls","romance curls","spanish curls","afro curls","wavy curls","human hair curls",
+    "deep wave curls","peruvian curls"
+],
+}
+console.log(typesOfHair.namesOfHairs [0])
+console.log(typesOfHair.namesOfHairs[1])
+console.log(typesOfHair.namesOfHairs[2])
+console.log(typesOfHair.namesOfHairs[3])
+console.log(typesOfHair.namesOfHairs[4])
+console.log(typesOfHair.namesOfHairs[5])
+console.log(typesOfHair.namesOfHairs[6])
+console.log(typesOfHair.namesOfHairs[7])
+console.log(typesOfHair.namesOfHairs[8])
+console.log(typesOfHair.namesOfHairs[9])
+console.log(typesOfHair.namesOfHairs[10])
+console.log(typesOfHair.namesOfHairs[11])
+console.log(typesOfHair.namesOfHairs[12])
+console.log(typesOfHair.namesOfHairs[13])
+console.log(typesOfHair.namesOfHairs[14])
+
+console.log(typesOfHair.colorsOfHairs[7])
+console.log(typesOfHair.colorsOfHairs[8])
+console.log(typesOfHair.colorsOfHairs[9])
+console.log(typesOfHair.colorsOfHairs[10])
+console.log(typesOfHair.colorsOfHairs[11])
+console.log(typesOfHair.colorsOfHairs[12])
+console.log(typesOfHair.colorsOfHairs[13])
+console.log(typesOfHair.colorsOfHairs[14])
+console.log(typesOfHair.colorsOfHairs[0])
+console.log(typesOfHair.colorsOfHairs[1])
+console.log(typesOfHair.colorsOfHairs[2])
+console.log(typesOfHair.colorsOfHairs[3])
+console.log(typesOfHair.colorsOfHairs[4])
+console.log(typesOfHair.colorsOfHairs[5])
+console.log(typesOfHair.colorsOfHairs[6])
+
+console.log(typesOfHair.typesOfCurls[3])
+console.log(typesOfHair.typesOfCurls[2])
+console.log(typesOfHair.typesOfCurls[1])
+console.log(typesOfHair.typesOfCurls[0])
+console.log(typesOfHair.typesOfCurls[4])
+console.log(typesOfHair.typesOfCurls[5])
+console.log(typesOfHair.typesOfCurls[6])
+console.log(typesOfHair.typesOfCurls[7])
+console.log(typesOfHair.typesOfCurls[8])
+console.log(typesOfHair.typesOfCurls[9])
+console.log(typesOfHair.typesOfCurls[10])
+console.log(typesOfHair.typesOfCurls[11])
+console.log(typesOfHair.typesOfCurls[12])
+console.log(typesOfHair.typesOfCurls[13])
+console.log(typesOfHair.typesOfCurls[14])
+
+
+
+// create if or else statements 
+const score=60
+if(score<49){console.log("fail")}
+else if(score<59){console.log("good")}
+else if(score<69){console.log("very good")}
+else if(score>69){console.log("excellent")}
+
